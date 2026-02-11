e49a36c7ad5f67265eb677d9fe3d17b3fe69c640
camillonunez1998 <camilo.nr14@gmail.com>
2026-02-11 16:58:12 +0100

    IaC update

--- tasks/infrastructure/main.tf
+++ tasks/infrastructure/main.tf
@@ -50,49 +50,6 @@ resource "aws_security_group" "k8s_sg" {
   }
 }
 
-# --- 3. Script de Inicialización (User Data) ---
-# Este script prepara el OS, instala containerd v1.35 y las herramientas de K8s
-locals {
-  k8s_setup = <<-EOF
-              #!/bin/bash
-              # Desactivar Swap
-              swapoff -a
-              sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-              # Modulos del Kernel
-              cat <<EOT | tee /etc/modules-load.d/k8s.conf
-              overlay
-              br_netfilter
-              EOT
-              modprobe overlay
-              modprobe br_netfilter
-
-              # Sysctl params
-              cat <<EOT | tee /etc/sysctl.d/k8s.conf
-              net.bridge.bridge-nf-call-iptables  = 1
-              net.bridge.bridge-nf-call-ip6tables = 1
-              net.ipv4.ip_forward                 = 1
-              EOT
-              sysctl --system
-
-              # Containerd con SystemdCgroup
-              apt-get update && apt-get install -y containerd
-              mkdir -p /etc/containerd
-              containerd config default | tee /etc/containerd/config.toml
-              sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-              systemctl restart containerd
-
-              # Instalar Kubeadm, Kubelet y Kubectl (v1.35)
-              apt-get update && apt-get install -y apt-transport-https ca-certificates curl gpg
-              mkdir -p /etc/apt/keyrings
-              curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-              echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-              apt-get update
-              apt-get install -y kubelet kubeadm kubectl
-              apt-mark hold kubelet kubeadm kubectl
-              EOF
-}
-
 # --- EC2 instances ---
 
 # Nodo Control Plane (Master)
@@ -102,7 +59,6 @@ resource "aws_instance" "master" {
   key_name      = "daily-key"
   vpc_security_group_ids = [aws_security_group.k8s_sg.id]
 
-  user_data = local.k8s_setup
 
   tags = { Name = "k8s-master" }
 }
@@ -114,8 +70,6 @@ resource "aws_instance" "workers" {
   instance_type = var.instance_type_worker
   key_name      = "daily-key"
   vpc_security_group_ids = [aws_security_group.k8s_sg.id]
-
-  user_data = local.k8s_setup
 
   tags = { Name = "k8s-worker-${count.index}" }
 }
